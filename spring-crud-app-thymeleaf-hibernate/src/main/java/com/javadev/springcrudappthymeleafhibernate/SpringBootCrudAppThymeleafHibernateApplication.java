3e8a8e993a29b2a367af752ec06a75263ebc2f21
MShahzodbek <mshahzodbek0309@gmail.com>
2023-06-02 22:53:50 +0500

    Created Spring Boot app

---
+++ spring-crud-app-thymeleaf-hibernate/src/main/java/com/javadev/springcrudappthymeleafhibernate/SpringBootCrudAppThymeleafHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.springcrudappthymeleafhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudAppThymeleafHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudAppThymeleafHibernateApplication.class, args);
+	}
+
+}
---
+++ spring-crud-app-thymeleaf-hibernate/src/test/java/com/javadev/springcrudappthymeleafhibernate/SpringBootCrudAppThymeleafHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.springcrudappthymeleafhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudAppThymeleafHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
